bd68276ebbacef9e0070fcb5cffef53083a5d661
Wiz Maven <157946355+WizMaven@users.noreply.github.com>
2025-01-16 17:50:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const popup = document.querySelector('.popup')
+const pop = document.querySelector('.pop')
+const hide = document.querySelector('.hide')
+
+hide.addEventListener("click", () => {
+    popup.style.display = "none";
+})
+
